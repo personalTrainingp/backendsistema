56bc1158f68dfd95281e88bb3fa59eb12afbef0c
personalTrainingp <crosales@personaltraining.com.pe>
2024-08-05 12:59:04 -0500

    comentar el anterior link

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ const checkMembresiaShips = () => {
 };
 
 const allowedOrigins = [
-  "https://personaltraining-sigma.vercel.app",
+  // "https://personaltraining-sigma.vercel.app",
   "https://change-the-slim-studio-sigma.vercel.app",
   "http://localhost:5173",
   "http://localhost:5174",
