dd1d3473c9929437990fde85c40bac968a6911d8
papopapota <daniel_olivares09@hotmail.com>
2024-10-22 18:12:25 -0500

    cambios terminados marcacion

--- routes/marcacion.router.js
+++ routes/marcacion.router.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 const {
   obtenerAsistenciaDeClientes,
+  obtenerAsistenciaPorClientes
 } = require("../controller/marcacion.controller");
 const { validarJWT } = require("../middlewares/validarJWT");
 const router = Router();
 
 router.get("/obtener-asistencia-clientes", obtenerAsistenciaDeClientes);
+
+router.get("/obtener-asistencia-clientes-por-cliente", obtenerAsistenciaPorClientes);
+
 
 module.exports = router;
